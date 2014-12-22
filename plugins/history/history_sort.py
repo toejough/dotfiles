8fd3438f3872c9890060229d6af48f7e74661125
toejough <toejough@gmail.com>
2014-12-22 11:06:23 -0700

    history dedup update - try to guard better against losing history, and some comment format updates

--- plugins/history/history_sort.py
+++ plugins/history/history_sort.py
@@ -1,15 +1,15 @@
 '''Sort bash history'''
 
-#  [ Imports ]
-#  [ -Python- ]
+# [ Imports ]
+# [ -Python- ]
 import sys
 import os.path
 
-#  [ Argument Parsing ]
+# [ Argument Parsing ]
 hist_file_path = sys.argv[-1]
 
 
-#  [ Helpers ]
+# [ Helpers ]
 def is_timestamp_line(line):
     if line.startswith('#'):
         try:
@@ -20,46 +20,54 @@ def is_timestamp_line(line):
     return False
 
 
-#  [ Main ]
-#  Safety check
+# [ Main ]
+# Safety check
 if not os.path.isfile(hist_file_path):
     print("{} cannot sort {}: it is not a regular file.".format(sys.argv[0], hist_file_path))
     exit(1)
 
 #  Save off modification time
 mtime = os.path.getmtime(hist_file_path)
-#  Get the current history
+# Get the current history
 with open(hist_file_path) as hist_file:
     lines = hist_file.readlines()
-#  Build sortable history list
+# Build sortable history list
 sortable = []
 for index, line in enumerate(lines):
     if not is_timestamp_line(line):
         timestamp_index = index - 1
         if timestamp_index >= 0 and is_timestamp_line(lines[timestamp_index]):
-            #  preceeded by a timestamp line.  Make a new object.
+            # preceeded by a timestamp line.  Make a new object.
             sortable.append({
                 "ts": lines[timestamp_index],
                 "commands": [line]
             })
         elif sortable:
-            #  no timestamp, but a prior command had one
+            # no timestamp, but a prior command had one
             sortable[-1]["commands"].append(line)
         else:
-            #  no timestamp, first command in history
+            # no timestamp, first command in history
             sortable.append({
                 "ts": "#0\n",
                 "commands": [line]
             })
-#  Sort
+# Sort
 sortable.sort(key=lambda i: i["ts"])
-#  Build new string
+# Build new string
 new_lines = []
 for item in sortable:
     new_lines.append(item["ts"])
     for command in item["commands"]:
         new_lines.append(command)
-#  Guard a little against file access issues:
+# Lines are getting lost and I don't know where.  Guard against that here.
+for line in lines:
+    if not is_timestamp_line(line):
+        if line not in new_lines:
+            sys.stderr.write("{} deduplicated {} incorrectly.  New file is missing '{}'.  Not writing new file.\n".format(
+                    sys.argv[0], hist_file_path, line
+            ))
+            exit(1)
+# Guard a little against file access issues:
 new_mtime = os.path.getmtime(hist_file_path)
 if mtime == new_mtime:
     with open(hist_file_path, 'w') as hist_file:

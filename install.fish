5d78ef00ea78f4eaebabc8f8517eb271ba9edb97
toejough <toejough@gmail.com>
2017-07-18 17:53:58 -0400

    adding alacritty linking line

--- install.fish
+++ install.fish
@@ -4,3 +4,4 @@
 mkdir -p ~/.config/fish
 ln -vFfs ~/.settings/.config/fish/* ~/.config/fish
 ln -vf ~/.settings/.config/fish/fishd.universal  ~/.config/fish/(ls ~/.config/fish | ack fishd | ack -v universal)
+ln -vFfs ~/.settings/.config/alacritty ~/.config/alacritty

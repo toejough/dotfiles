56362b3994bfdcb6a05eab181d686265af64dacd
toejough <toejough@gmail.com>
2018-03-08 09:08:35 -0500

    removing iterm2 integration hooks for setting badge values - badge doesn't work well with tmux and I'm using that more frequently now.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,4 @@
 # Shell integrations
-source ~/.iterm2_shell_integration.fish
-function iterm2_print_user_vars
-    set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
-    set -l venv (basename $VIRTUAL_ENV ^/dev/null)
-    set -l pyversion (python --version ^/dev/null; or echo "No python")
-    iterm2_set_user_var cur_dir (basename (pwd))
-    iterm2_set_user_var git_branch (test -n "$git_branch"; and echo "Branch: $git_branch"; or echo "No branch")
-    iterm2_set_user_var venv (test -n "$venv"; and echo "Venv: $venv"; or echo "No venv")
-    iterm2_set_user_var pyversion "$pyversion"
-end
 
 # Editor
 set -x EDITOR vim

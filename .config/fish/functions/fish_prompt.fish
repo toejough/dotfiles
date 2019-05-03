4fd0c4c53468945fd5444da4d9c0f71bc6747053
toejough <toejough@gmail.com>
2019-05-03 15:13:08 -0400

    adding pwd on change and RC

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,19 @@
 function fish_prompt
-    set -U last_dir (pwd)
-    echo "> "
+    set -l last_status $status
+
+    set -l current_dir (pwd)
+    if test $last_dir != $current_dir
+        set dir_text (set_color normal)"| PWD: "(set_color blue)"$current_dir "
+        set -U last_dir $current_dir
+    else
+        set dir_text ""
+    end
+
+    if test $last_status -eq 0
+        set rc_text "RC: "(set_color green)"$last_status "
+    else
+        set rc_text "RC: "(set_color red)"$last_status "
+    end
+
+    echo "$rc_text$dir_text"(set_color normal)"> "
 end

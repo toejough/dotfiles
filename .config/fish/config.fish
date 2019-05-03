c5ce7be4e1bf803176e6592db28c37b47f4ad28d
toejough <toejough@gmail.com>
2019-05-03 14:31:00 -0400

    removed python-specific things from config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,5 @@
 # Editor
 set -x EDITOR vim
 
-# added for pipx (https://github.com/cs01/pipx)
-set -x PATH /Users/joe/.local/bin $PATH
-
-# added by pipx
-set -x PATH /Users/joe/.local/bin $PATH
-
-# pyenv activation hooks
-source (pyenv init -| psub)
-source (pyenv virtualenv-init -| psub)
-set -x PYENV_ROOT /Users/joe/.pyenv
-set -x PYTHON_CONFIGURE_OPTS "--enable-framework"
-
 # CD to last dir
 cd $last_dir

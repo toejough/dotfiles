396fe8d3342de8a4d078dc0cdb4e6f5a3be39110
toejough <toejough@gmail.com>
2019-08-30 11:16:22 -0400

    running fish customizations only on interactive shells

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,40 +1,43 @@
-# Editor
-# Checking if we have already inherited an editor
-if ! set -q EDITOR
-    # if not, use Vim
-    set -x EDITOR vim
+if status --is-interactive
+    # Editor
+    # Checking if we have already inherited an editor
+    if ! set -q EDITOR
+        # if not, use Vim
+        set -x EDITOR vim
+    end
+
+    # keybindings
+    fish_vi_key_bindings
+
+    # Set dirnext and dirprev and direction to universals
+    set -U dirprev $dirprev
+    set -U dirnext $dirnext
+    set -U __fish_cd_direction $__fish_cd_direction
+
+    # set initial directories
+    if not set -q last_dir
+        set -U last_dir (pwd)
+    end
+
+    # Enable post-exec
+    source ~/dotfiles/.config/fish/functions/post_exec.fish
+
+    # CD to last dir
+    cd $last_dir
+
+    # Prevent the press-and-hold key behavior
+    defaults write -g ApplePressAndHoldEnabled -bool false
+
 end
-
-# keybindings
-fish_vi_key_bindings
-
-# Set dirnext and dirprev and direction to universals
-set -U dirprev $dirprev
-set -U dirnext $dirnext
-set -U __fish_cd_direction $__fish_cd_direction
-
-# set initial directories
-if not set -q last_dir
-    set -U last_dir (pwd)
-end
-
-# Enable post-exec
-source ~/dotfiles/.config/fish/functions/post_exec.fish
-
-# CD to last dir
-cd $last_dir
 
 # Update path to include go binaries
 set PATH ~/go/bin:$PATH
+#
+# Update path to include local binaries
+set PATH ~/.local/bin:$PATH
 
 # Update envvars per directory
 direnv hook fish | source
 
-# Prevent the press-and-hold key behavior
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '~/google-cloud-sdk/path.fish.inc' ]; . '~/google-cloud-sdk/path.fish.inc'; end
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-

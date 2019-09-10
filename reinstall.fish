133f94b0f4f0bda9c265e8b7af24d130dbe22f22
toejough <toejough@gmail.com>
2019-09-09 22:34:55 -0400

    adding fish reinstall script

---
+++ reinstall.fish
@@ -0,0 +1,24 @@
+#! /usr/bin/env fish
+
+# fish configs
+mkdir -p ~/.config/fish/functions
+ln -vFfs ~/dotfiles/.config/fish/config.fish ~/.config/fish
+ln -vFfs ~/dotfiles/.config/fish/functions/* ~/.config/fish/functions
+# other configs
+ln -vFfs ~/dotfiles/.vimrc ~
+ln -vFfs ~/dotfiles/.gitconfig ~
+ln -vFfs ~/dotfiles/.tmux.conf ~
+# nvim configs
+mkdir -p ~/.config/nvim
+ln -vFfs ~/dotfiles/.config/nvim/* ~/.config/nvim
+
+# brew-installable programs
+brewm update
+
+# fish plugins
+fisher --self-update
+fisher rm (fisher ls)
+fisher add (cat ~/dotfiles/fisher-list.txt)
+
+# vim plugins
+vim -c ReloadRC

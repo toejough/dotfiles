7d0ff77e372360cc67b3aacf4f052efe2d37ceb5
toejough <toejough@gmail.com>
2019-09-19 13:26:58 -0400

    fix up indentation and add status output to git-report

--- .config/fish/functions/git-report.fish
+++ .config/fish/functions/git-report.fish
@@ -1,16 +1,17 @@
-# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.ixC0O6/git-report.fish @ line 2
+# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.XozMZj/git-report.fish @ line 2
 function git-report
 	clear
+	git status -sb
 
-     echo -e "\nBRANCH\n"
-     git log --oneline branch-base.(git name)..local-base.(git name) --color --decorate --stat --reverse | cat
+    echo -e "\nBRANCH\n"
+    git log --oneline branch-base.(git name)..local-base.(git name) --color --decorate --stat --reverse | cat
 
-     echo -e "\nLOCAL\n"
-     git log --oneline local-base.(git name)..HEAD --color --decorate --stat --reverse | cat
+    echo -e "\nLOCAL\n"
+    git log --oneline local-base.(git name)..HEAD --color --decorate --stat --reverse | cat
 
-     echo -e "\nSTAGED\n"
-     git diff --color --staged | diff-so-fancy
+    echo -e "\nSTAGED\n"
+    git diff --color --staged | diff-so-fancy
 
-     echo -e "\nUNSTAGED\n"
-     git diff --color | diff-so-fancy
+    echo -e "\nUNSTAGED\n"
+    git diff --color | diff-so-fancy
 end

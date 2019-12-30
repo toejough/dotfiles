773045991c5da3bcc85b15ca56c537163ba15a40
toejough <toejough@gmail.com>
2019-12-30 08:15:04 -0500

    stop sorting iv

--- .config/fish/functions/iv.fish
+++ .config/fish/functions/iv.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.IB7MVz/iv.fish @ line 2
+# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.gccGpd/iv.fish @ line 2
 function iv --description 'nvim open file chosen by fzf from most recently nvim-opened files'
-	nvim (begin; for f in (vim -e -c 'echo v:oldfiles|qall' --headless -u NONE 2>&1 | tr "'" '"' | sed 's/""//g' | jq .[] -r | sort -u); if test -f $f; echo $f; end; end; end | fzf)
+	nvim (begin; for f in (vim -e -c 'echo v:oldfiles|qall' --headless -u NONE 2>&1 | tr "'" '"' | sed 's/""//g' | jq .[] -r ); if test -f $f; echo $f; end; end; end | fzf)
 end

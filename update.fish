ecf51900b3b2d2a61207f3d40095149691d24b1e
toejough <toejough@gmail.com>
2020-02-29 11:43:14 -0500

    [OTHER] pulling latest dotfiles on update

--- update.fish
+++ update.fish
@@ -1,42 +1,48 @@
 #! /usr/bin/env fish
 
-# fish configs
-mkdir -p ~/.config/fish/functions
-and ln -vFfs ~/dotfiles/.config/fish/config.fish ~/.config/fish
-and ln -vFfs ~/dotfiles/.config/fish/functions/* ~/.config/fish/functions
-# other configs
-and ln -vFfs ~/dotfiles/.vimrc ~
-and ln -vFfs ~/dotfiles/.gitconfig ~
-and ln -vFfs ~/dotfiles/.tmux.conf ~
-# nvim configs
-and mkdir -p ~/.config/nvim
-and ln -vFfs ~/dotfiles/.config/nvim/* ~/.config/nvim
-# alacritty configs
-and mkdir -p ~/.config/alacritty
-and ln -vFfs ~/dotfiles/.config/alacritty/* ~/.config/alacritty
+# get the latest
+if cd ~/dotfiles; and git fetch; and git status | grep -i 'your branch is up to date'
+    # fish configs
+    mkdir -p ~/.config/fish/functions
+    and ln -vFfs ~/dotfiles/.config/fish/config.fish ~/.config/fish
+    and ln -vFfs ~/dotfiles/.config/fish/functions/* ~/.config/fish/functions
+    # other configs
+    and ln -vFfs ~/dotfiles/.vimrc ~
+    and ln -vFfs ~/dotfiles/.gitconfig ~
+    and ln -vFfs ~/dotfiles/.tmux.conf ~
+    # nvim configs
+    and mkdir -p ~/.config/nvim
+    and ln -vFfs ~/dotfiles/.config/nvim/* ~/.config/nvim
+    # alacritty configs
+    and mkdir -p ~/.config/alacritty
+    and ln -vFfs ~/dotfiles/.config/alacritty/* ~/.config/alacritty
 
-# brew-installable programs
-and brewm update
+    # brew-installable programs
+    and brewm update
 
-# fish plugins
-and fisher self-update
-and fisher ls | fisher rm
-and cat ~/dotfiles/fisher-list.txt | fisher add
+    # fish plugins
+    and fisher self-update
+    and fisher ls | fisher rm
+    and cat ~/dotfiles/fisher-list.txt | fisher add
 
-# vim plugins
-and pip3 install neovim
-and vim +ReloadRC  # don't quit - want to see what got updated sometimes
-and vim -c "CocInstall coc-json coc-diagnostic" +qall
+    # vim plugins
+    and pip3 install neovim
+    and vim +ReloadRC  # don't quit - want to see what got updated sometimes
+    and vim -c "CocInstall coc-json coc-diagnostic" +qall
 
-# go tooling
-and go get golang.org/x/tools/gopls@latest
-and go get golang.org/x/tools/cmd/goimports
+    # go tooling
+    and go get golang.org/x/tools/gopls@latest
+    and go get golang.org/x/tools/cmd/goimports
 
-# lsp stuff
-and npm install -g npm
-and npm install -g yarn
-and yarn global add diagnostic-languageserver
+    # lsp stuff
+    and npm install -g npm
+    and npm install -g yarn
+    and yarn global add diagnostic-languageserver
 
-# final message
-and echo "Updates completed!!"
-or echo "Update FAILED (╯°□°）╯︵ ┻━┻ "
+    # final message
+    and echo "Updates completed!!"
+    or echo "Update FAILED (╯°□°）╯︵ ┻━┻ "
+else
+    git pull
+    and update.fish
+end

67daf6704af2f7577db185f8dbb2b44aeb09b35d
toejough <toejough@gmail.com>
2020-03-02 08:31:18 -0500

    remove unnecessary iv

--- .config/fish/functions/iv.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.gccGpd/iv.fish @ line 2
-function iv --description 'nvim open file chosen by fzf from most recently nvim-opened files'
-	nvim (begin; for f in (vim -e -c 'echo v:oldfiles|qall' --headless -u NONE 2>&1 | tr "'" '"' | sed 's/""//g' | jq .[] -r ); if test -f $f; echo $f; end; end; end | fzf)
-end

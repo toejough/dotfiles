eacbf3994190ad388c1dadb2b8bffc9a5ccea2ee
toejough <toejough@gmail.com>
2020-04-07 21:27:53 -0400

    [OTHER] improve fzf preview in ic

--- .config/fish/functions/ic.fish
+++ .config/fish/functions/ic.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.BnGOKX/ic.fish @ line 1
+# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.Nap1FE/ic.fish @ line 2
 function ic --description 'cd to a recent directory as chosen with z and fzf'
-	cd (z -l | fzf | awk '{print $NF}')
+	cd (z -l | fzf --preview 'lt {-1}' --preview-window down | awk '{print $NF}')
 end

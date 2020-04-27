f6bbea6eca84557f5deddfc37d2cde189b31143d
toejough <toejough@gmail.com>
2020-04-27 07:43:33 -0400

    allowing ic to take filter args

--- .config/fish/functions/ic.fish
+++ .config/fish/functions/ic.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.Nap1FE/ic.fish @ line 2
-function ic --description 'cd to a recent directory as chosen with z and fzf'
-	cd (z -l | fzf --preview 'lt {-1}' --preview-window down | awk '{print $NF}')
+# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.TUKINY/ic.fish @ line 2
+function ic --wraps='z -l' --description 'cd to a recent directory as chosen with z and fzf'
+	cd (z -l $argv | fzf --preview 'lt {-1}' --preview-window down | awk '{print $NF}')
 end

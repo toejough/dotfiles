9c6b8cee73765ceed1d242940318b0d075f9776f
toejough <toejough@gmail.com>
2021-04-05 08:22:38 -0400

    [OTHER] various brew updates for packages and the updated installer script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 # homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # fish
 brew install fish

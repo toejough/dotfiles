b1b58f70e49751bf6316617d520fb521ef49736b
toejough <toejough@gmail.com>
2023-01-07 16:20:35 -0500

    feat(install): describe what's being installed

--- install.fish
+++ install.fish
@@ -1,7 +1,13 @@
 #! /usr/bin/env fish
 
 # fish plugins
+echo "Installing fisher..."
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
+# expected prereqs to the update script
+echo "Installing ack..."
+brew install ack
+
 # update dirs/configs/packages
+echo "Running update.fish..."
 ~/dotfiles/update.fish

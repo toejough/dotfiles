ee231e5869b9386572706d7db5372d34090197e4
toejough <toejough@gmail.com>
2024-03-09 23:09:27 -0500

    feat: adding lsp's, autocompletion, and format-on-save with said LSP's

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,17 +26,121 @@ require("lazy").setup(
 			end,
 		},
 		{ "folke/which-key.nvim", lazy = true },
+		-- autosave
+		{
+			"907th/vim-auto-save",
+			config = function()
+				vim.g.auto_save = 1
+			end,
+		},
+		-- auotoread
+		"djoshea/vim-autoread",
+		-- lsp's & such
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"folke/neodev.nvim",
+		-- completion
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 	},
 	-- try to load one of these colorschemes when starting an installation during startup
-	{install = {colorscheme = { "solarized" }}}
+	{ install = { colorscheme = { "solarized" } } }
 )
+
+-- Set up leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set up some keymaps
 local wk = require("which-key")
-wk.register({["<"] = { "<gv", "unindent"}}, {mode="v"})
-wk.register({[">"] = { ">gv", "indent"}}, {mode="v"})
+wk.register({ ["<"] = { "<gv", "unindent" } }, { mode = "v" })
+wk.register({ [">"] = { ">gv", "indent" } }, { mode = "v" })
 
 -- make all tabs and indents 4 spaces
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+-- mason setup for installing lsp servers
+require("neodev").setup()          -- allows neovim autocompletion
+require("mason").setup()           -- lsp server management
+require("mason-lspconfig").setup() -- lsp config help
+
+-- format on save with configured LSP's
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+-- set up completion
+local cmp = require('cmp')
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' }, -- For luasnip users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
+
+-- set up each LSP with completion by cmp_nvim_lsp	
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require("mason-lspconfig").setup_handlers {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup {
+			capabilities = capabilities
+		}
+	end,
+	-- Next, you can provide a dedicated handler for specific servers.
+	-- For example, a handler override for the `rust_analyzer`:
+	--["rust_analyzer"] = function ()
+	--    require("rust-tools").setup {}
+	--end
+}

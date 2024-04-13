50c747ef65a20443b9279351ca2190abacd79759
toejough <toejough@gmail.com>
2024-04-13 10:50:03 -0400

    feat(fish): set last dir on greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,6 +1,10 @@
 # Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.LReYi6/fish_greeting.fish @ line 2
 function fish_greeting
 	clear
-    echo (set_color cyan)"PWD: "(set_color normal)(pwd)
-    echo
+	set -l current_dir (pwd)
+	if test $last_dir != $current_dir
+		set -U last_dir $current_dir
+	end
+	echo (set_color cyan)"PWD: "(set_color blue)"$current_dir"(set_color normal)
+	lt
 end

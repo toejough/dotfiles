458b64ea8c062091ae53d7255293d8abb8bd4527
toejough <toejough@gmail.com>
2024-04-15 09:08:01 -0400

    format(brewm): Reformat spacing for redirects in brewm.

--- .config/fish/functions/brewm-update.fish
+++ .config/fish/functions/brewm-update.fish
@@ -1,6 +1,6 @@
 # Defined in /var/folders/94/q46vzxqd5855jk2zbwqj5mbm0000gn/T//fish.ohIDmx/brewm-update.fish @ line 2
 function brewm-update
-    if not count $argv > /dev/null
+    if not count $argv >/dev/null
         brewm-update recipes; or return 1
         brewm-update casks; or return 1
         brewm-update taps; or return 1
@@ -16,7 +16,7 @@ function brewm-update
             set desired_brew_tap_list (cat ~/dotfiles/brew-tap-list.txt | awk '{print $1}' | sed -e '/^#/d')
             for tap in $brew_tap_list
                 echo -n "  found $tap..."
-                if not echo $desired_brew_tap_list | ack $tap > /dev/null
+                if not echo $desired_brew_tap_list | ack $tap >/dev/null
                     echo -n "not desired.  Untapping..."
                     brew untap $tap; or return 1
                     echo "done!"
@@ -25,7 +25,7 @@ function brewm-update
                 end
             end
             for tap in $desired_brew_tap_list
-                if not echo $brew_tap_list | ack $tap > /dev/null
+                if not echo $brew_tap_list | ack $tap >/dev/null
                     echo -n "  $tap not found.  Tapping..."
                     brew tap $tap; or return 1
                     echo "done!"
@@ -40,9 +40,9 @@ function brewm-update
             set desired_brew_list (cat ~/dotfiles/brew-recipe-list.txt | sed -e '/^#/d' | awk '{print $1}')
             for recipe in $brew_list
                 echo -n "  found $recipe..."
-                if not echo $desired_brew_list | ack $recipe > /dev/null
+                if not echo $desired_brew_list | ack $recipe >/dev/null
                     echo -n "not desired.  Uninstalling..."
-                    brew rmtree $recipe; or return 1
+                    brew rm $recipe; or return 1
                     echo "done!"
                 else
                     echo "great!"
@@ -54,7 +54,7 @@ function brewm-update
             set desired_brew_list (cat ~/dotfiles/brew-recipe-list.txt | sed -e '/^#/d' | gsed -E 's/\s*#.*//')
             for recipe in $desired_brew_list
                 set recipe_name (echo $recipe | awk '{print $1}')
-                if not echo $brew_list | ack $recipe_name > /dev/null
+                if not echo $brew_list | ack $recipe_name >/dev/null
                     echo -n "  $recipe not found.  Installing..."
                     eval brew install $recipe; or return 1
                     echo "done!"
@@ -70,7 +70,7 @@ function brewm-update
             set desired_brew_cask_list (cat ~/dotfiles/brew-cask-list.txt | awk '{print $1}' | sed -e '/^#/d')
             for cask in $brew_cask_list
                 echo -n "  found $cask..."
-                if not echo $desired_brew_cask_list | ack $cask > /dev/null
+                if not echo $desired_brew_cask_list | ack $cask >/dev/null
                     echo -n "not desired.  Uninstalling..."
                     brew uninstall --cask $cask; or return 1
                     echo "done!"
@@ -79,7 +79,7 @@ function brewm-update
                 end
             end
             for cask in $desired_brew_cask_list
-                if not echo $brew_cask_list | ack $cask > /dev/null
+                if not echo $brew_cask_list | ack $cask >/dev/null
                     echo -n "  $cask not found.  Installing..."
                     brew install --cask $cask; or return 1
                     echo "done!"

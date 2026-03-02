619cbca7a00710b9131e8145b9f8387ace39c458
toejough <toejough@gmail.com>
2026-03-02 16:50:55 -0500

    refactor(fish): remove unused ic function
    The ic (interactive cd) function used z+fzf for directory jumping.
    Zoxide's built-in interactive mode (zi) provides the same workflow
    without a custom wrapper.

--- .config/fish/functions/ic.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /var/folders/v5/mgpjg7ms68n_tb2mfljgy1d40000gn/T//fish.TUKINY/ic.fish @ line 2
-function ic --wraps='z -l' --description 'cd to a recent directory as chosen with z and fzf'
-	cd (z -l $argv | fzf --preview 'lt {-1}' --preview-window down | awk '{print $NF}')
-end
